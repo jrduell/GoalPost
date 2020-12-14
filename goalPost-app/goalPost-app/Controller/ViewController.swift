f4e490c24d617f0d3b397bf9e96ecd7c95d09b17
jrduell <jrduell7@gmail.com>
2020-12-14 07:49:38 -0600

    project creation

---
+++ goalPost-app/goalPost-app/Controller/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  goalPost-app
+//
+//  Created by Jacob Duell on 12/14/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
