8ec08a2957af648d236225d3a7d23d6f2181e0ce
jrduell <jrduell7@gmail.com>
2020-12-16 08:09:08 -0600

    present data in goals table view controller

---
+++ goalPost-app/goalPost-app/Enums/GoalType.swift
@@ -0,0 +1,13 @@
+//
+//  GoalType.swift
+//  goalPost-app
+//
+//  Created by Jacob Duell on 12/16/20.
+//
+
+import Foundation
+
+enum GoalType: String {
+    case longTerm = "Long Term"
+    case shortTerm = "Short Term"
+}
